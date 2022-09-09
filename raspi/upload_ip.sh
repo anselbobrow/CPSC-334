e6498561ecbd83bb0c29ea2e121ede6b74ac58d3
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-09-09 12:52:04 -0400

    add logging to upload_ip.sh

--- raspi/upload_ip.sh
+++ raspi/upload_ip.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 cd /home/student334/Desktop/CPSC-334
-if ! git diff-index --quiet HEAD --; then
-	git add raspi/ip.md
-	git commit -m "update ip"
-	git push
-fi
 
-echo "script was triggered by `whoami`" > /home/student334/Desktop/log.txt
-hostname -I >> /home/student334/Desktop/log.txt
+echo "entering directory:" > log.txt
+pwd >> log.txt
+
+echo "saving ip address: " >> log.txt
+hostname -I >> log.txt
+
+hostname -I >> raspi/ip.md
+
+echo "pushing to git..." >> log.txt
+git add raspi/ip.md
+git commit --allow-empty -m "update ip"
+git push
+
+echo "script was successfully triggered by $(whoami)" > log.txt
 
 cd -
-exit 0
