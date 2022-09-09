11d3a97dc168021ae318141d7ba0d2246bf2487a
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-09-09 12:33:10 -0400

    add gitignore, update upload_ip.sh

--- raspi/upload_ip.sh
+++ raspi/upload_ip.sh
@@ -2,11 +2,11 @@
 
 cd /home/student334/Desktop/CPSC-334
 if ! git diff-index --quiet HEAD --; then
-	git add ip.md
+	git add raspi/ip.md
 	git commit -m "update ip"
 	git push
 fi
 cd -
 
-echo "script was triggered by ${whoami}" > log.txt
-$ip_address >> log.txt
+echo "script was triggered by `whoami`" > /home/student334/Desktop/log.txt
+hostname -I >> log.txt
