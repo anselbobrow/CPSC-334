c62c6fa7b6af55f155aa46a7ae59cfeb15ec822f
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-09-15 15:46:56 -0400

    update upload_ip.sh

--- raspi/upload_ip.sh
+++ raspi/upload_ip.sh
@@ -2,19 +2,10 @@
 
 cd /home/student334/Desktop/CPSC-334
 
-echo "entering directory:" > log.txt
-pwd >> log.txt
+git pull
 
-echo "saving ip address: " >> log.txt
-hostname -I >> log.txt
+hostname -I > raspi/ip.md
 
-hostname -I >> raspi/ip.md
-
-echo "pushing to git..." >> log.txt
 git add raspi/ip.md
-git commit --allow-empty -m "update ip"
+git commit -m "update ip"
 git push
-
-echo "script was successfully triggered by $(whoami)" >> log.txt
-
-cd -
