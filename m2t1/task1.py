0adce19eb2900b4c094bcbce5844e150d368f17a
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-09-28 13:01:36 -0400

    finish mode 1:

--- m2t1/task1.py
+++ m2t1/task1.py
@@ -1,49 +1,75 @@
 #!/usr/bin/env python
 
 import os
+import curses
 from time import sleep
 from sys import stdout
 from gpiozero import Button, PWMLED
 
+# handle GPIO
 button = Button(2)
 switch = Button(3)
 joystick = Button(4)
 joystick_x = Button(5)
 joystick_y = Button(6)
 
-mode = 0
 
-
+# initialize mode and change_mode functions
 def change_mode():
     global mode
     mode = (mode + 1) % 3
 
 
+mode = 0
 button.when_activated = change_mode
 
-term_len = os.get_terminal_size().columns
-pos = 0
-stdout.write(' ' * term_len)
-while True:
-    stdout.write('\b' * term_len + '#' * pos + 'O' + '#' *
-                 (term_len - pos - 1))
-    stdout.flush()
-    sleep(0.1)
-    if mode == 0:
-        if not joystick_x.is_pressed:
-            if pos < term_len - 1:
-                pos += 1
-        else:
-            if 0 < pos:
-                pos -= 1
-    if mode == 1:
-        if joystick_y.is_pressed:
-            if pos < term_len - 1:
-                pos += 1
-        else:
-            if 0 < pos:
-                pos -= 1
 
+def main(stdscr):
+    # curses
+    stdscr.clear()
+    curses.curs_set(0)
+    term_len = curses.COLS
+    term_height = curses.LINES
+
+    pos_x = term_len - 2
+    pos_y = term_height - 2
+    while True:
+        if mode == 0:
+            # mode 1: move circle around screen across x, y coordinates, switch controls character
+            # draw grid of #'s with 'O' as cursor
+            for i in range(pos_y):
+                stdscr.addstr(i, 0, '#' * term_len)
+            # stdscr.addstr(pos_y, 0,
+            #               '#' * pos_x + 'O' + '#' * (term_len - pos_x - 1))
+            stdscr.addstr(pos_y, 0, '#' * pos_x)
+            stdscr.addstr(pos_y, pos_x, 'O')
+            stdscr.addstr(pos_y, pos_x + 1, '#' * (term_len - pos_x - 1))
+            for i in range(pos_y + 1, term_height):
+                stdscr.addstr(i, 0, '#' * (term_len - 1))
+
+            if joystick_x.is_active:
+                if 0 < pos_x:
+                    pos_x -= 1
+            else:
+                if pos_x < term_len - 2:
+                    pos_x += 1
+
+            if joystick_y.is_active:
+                if 0 < pos_y:
+                    pos_y -= 1
+            else:
+                if pos_y < term_height - 2:
+                    pos_y += 1
+        if mode == 1:
+            # mode 2
+        if mode == 2:
+            continue
+            # mode 3
+        stdscr.refresh()
+        sleep(0.1)
+
+
+curses.wrapper(main)
 # while True:
 #    if button.is_pressed:
 #        print("Button is pressed")
