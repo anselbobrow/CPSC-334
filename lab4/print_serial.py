2cd8f0c64a98f93aee049772442d59a97db8765a
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-10-11 19:06:10 -0400

    fix inconsistent quotes

--- lab4/print_serial.py
+++ lab4/print_serial.py
@@ -4,7 +4,7 @@ import serial
 from time import sleep 
 
 # include a timeout if there are no \n chars in the serial output
-esp = serial.Serial('/dev/cu.usbserial-10', 115200, timeout=0)
+esp = serial.Serial("/dev/cu.usbserial-10", 115200, timeout=0)
 
 while True:
     print(esp.readline().decode("utf-8"), end="")
