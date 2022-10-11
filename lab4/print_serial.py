e374d656431131325eae07833d04cac84aa7029d
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-10-11 19:04:23 -0400

    add lab4 code

---
+++ lab4/print_serial.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import serial
+from time import sleep 
+
+# include a timeout if there are no \n chars in the serial output
+esp = serial.Serial('/dev/cu.usbserial-10', 115200, timeout=0)
+
+while True:
+    print(esp.readline().decode("utf-8"), end="")
+    # sleep(0.1) # no sleep needed if timeout=0
