700b58cede919e85ecda6da24ca0f9fdcc04b031
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-10-13 14:55:33 -0400

    add mac address file

--- lab4/print_serial.py
+++ lab4/print_serial.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import serial
-from time import sleep 
+from time import sleep
 
 # include a timeout if there are no \n chars in the serial output
-esp = serial.Serial("/dev/cu.usbserial-10", 115200, timeout=0)
+esp = serial.Serial("/dev/ttyUSB0", 115200, timeout=0)
 
 while True:
     print(esp.readline().decode("utf-8"), end="")
