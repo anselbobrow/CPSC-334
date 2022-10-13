cbe165b295cc4227c70696eb1afb423bcfec78ae
Ansel Bobrow <ansel.bobrow@gmail.com>
2022-10-13 15:58:24 -0400

    add photoresistor.py

---
+++ lab6/photoresistor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import socket
+
+UDP_IP = "172.29.31.240"
+UDP_PORT = 5070
+
+sock = socket.socket(
+    socket.AF_INET,  # Internet
+    socket.SOCK_DGRAM)  # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    print("received message: %s" % data.decode("ASCII"))
